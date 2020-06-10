96f1a2bd02e353588f429669eadaf0ec82839df5
swativar <64944666+swativar@users.noreply.github.com>
2020-06-10 14:56:52 +0530

    Add files via upload

---
+++ pet store/index.js
@@ -0,0 +1,30 @@
+
+$(document).ready(function(){
+    M.AutoInit();
+    $(".cart_click").click(function(){
+        M.toast({html:'Added to cart',completeCallback: function(){alert('Go to Cart to complete process..')}});
+    })
+  });
+$(".form_in").submit(function(e){
+    e.preventDefault();
+var xhttp = new XMLHttpRequest();
+xhttp.addEventListener("readystatechange",function(){
+    if(this.readyState==4 && this.status==200){
+        $("html").html(this.responseText);
+    }
+})
+xhttp.open("GET","Thank.html",true);
+xhttp.send();
+})
+$("#show").hide();
+$("#form_get").submit(function(e){
+    e.preventDefault();
+    $("#form_hide").hide();
+    $("#show").show();
+});
+$("#form_s_show").hide();
+$("#form_s").submit(function(e){
+    e.preventDefault();
+    $("#form_s_hide").hide();
+    $("#form_s_show").show();
+});
